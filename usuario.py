eea542c8e8c0ad2be361cb9e27552d22dbb43d28
Karen Limari <karenlimari20@gmail.com>
2024-09-07 00:51:11 +0200

    Agregar Readme

--- usuario.py
+++ usuario.py
@@ -1,4 +1,21 @@
 class Usuario():
+    """
+    Representa un usuario con información básica.
+
+    Esta clase se utiliza para almacenar los detalles de un usuario, incluyendo 
+    el nombre, apellido, email y género.
+
+    Parámetros
+    ----------
+    nombre : str
+        El nombre del usuario.
+    apellido : str
+        El apellido del usuario.
+    email : str
+        La dirección de correo electrónico del usuario.
+    genero : str
+        El género del usuario.
+    """
     def __init__(self, nombre: str, apellido: str, email: str, genero: str) -> None:
         self.nombre = nombre
         self.apellidos = apellido

67e0133251c317af1e80c47fe77f7e3060030f5e
Karen Limari <karenlimari20@gmail.com>
2024-09-06 16:39:21 +0200

    Agregar instancia usuario y condición if

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 import json
 from usuario import Usuario
 
-#Función para leer el archivo usuario.txt
-def leer_archivo(usuario.txt, error.log):
+#Función para procesar los usuarios en el archivo usuario.txt y captar errores
+def procesar_usuarios(usuario.txt, error.log):
     usuarios = [] #Se crea esta lista vacía para almacenar los datos de los usuarios
 
 #Abre el archivo em modo leer

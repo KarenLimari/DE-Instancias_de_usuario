9f836bb66f9ffaa9746e74df7067818eb684315e
Karen Limari <karenlimari20@gmail.com>
2024-09-06 15:55:25 +0200

    Agregar def leer_archivo

---
+++ script.py
@@ -0,0 +1,6 @@
+import json
+from usuario import Usuario
+
+#Función para leer el archivo usuario.txt
+def leer_archivo(usuario.txt, error.log):
+    usuarios = [] #Se crea esta lista vacía para almacenar los datos de los usuarios
